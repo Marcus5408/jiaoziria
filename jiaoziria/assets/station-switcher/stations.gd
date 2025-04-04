5cea04fb076d4064c5d423cb294549632bfe323e
Issac Liu <issac@matchatea.dev>
2025-04-04 10:30:08 +0800

    feat: change station switcher to use `TextureButton` instead of regular `Button`

--- jiaoziria/assets/station-switcher/stations.gd
+++ jiaoziria/assets/station-switcher/stations.gd
@@ -6,7 +6,6 @@ signal station_changed_to_cook
 signal station_changed_to_sauce
 
 func _on_order_pressed() -> void:
-    
     emit_signal("station_changed_to_order")
 
 func _on_prepare_pressed() -> void:
@@ -21,3 +20,6 @@ func _on_sauce_pressed() -> void:
 
 func _on_store_view_customer_take_order_button_pressed(_customerNodeData:Variant, _customerName:String) -> void:
     self.hide()
+
+func _on_ticket_layer_order_taken(_customerName:String) -> void:
+    self.show()
