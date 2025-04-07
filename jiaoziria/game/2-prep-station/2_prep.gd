8723da16e1b351565bee56e280f8df4936f77ac1
lubabanawla <lubabanawla@gmail.com>
2025-04-06 22:52:04 -0400

    Omg I hope this does not BREAK - added prep station functionality...

--- jiaoziria/game/2-prep-station/2_prep.gd
+++ jiaoziria/game/2-prep-station/2_prep.gd
@@ -1,3 +1,21 @@
 extends Control
 
 var dumpling_skin_active = false
+
+var red_filling = false
+
+@onready var finishedDumplingButton = get_node("finishDumpling")
+@onready var finishedDumplingImage = get_node("FinishedDumplingImage")
+@onready var IngredientsSomething = get_node("Ingredients")
+
+func filling_activate():
+	finishedDumplingButton.visible = true
+	red_filling = true
+
+func dumpling_activate():
+	dumpling_skin_active = true
+
+func _on_finish_dumpling_pressed() -> void:
+	if (red_filling == true and dumpling_skin_active == true):
+		finishedDumplingImage.visible = true
+		IngredientsSomething.visible = false
