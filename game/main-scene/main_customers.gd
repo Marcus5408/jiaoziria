6bbd581471b1e2e3e201b0e420af8fb96f63a20c
lubabanawla <lubabanawla@gmail.com>
2025-05-19 01:22:14 -0400

    reverted changes

--- game/main-scene/main_customers.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _input(event):
-	if event is InputEventKey and event.is_action_pressed("dev_spawn_customer"):
-		spawn_random_customer()
-
-func spawn_random_customer():
-	return
